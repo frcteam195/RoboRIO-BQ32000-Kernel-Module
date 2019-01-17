bd2fd34c4311af4b5ef3ea8c5b4ad8135de19525
Robert Hilton <robert.a.hilton.jr@gmail.com>
2019-01-17 17:25:06 -0500

    Update RoboRIOSetup.sh

--- RoboRIOSetup.sh
+++ RoboRIOSetup.sh
@@ -21,3 +21,9 @@ echo bq32000 0x68 | tee /sys/class/i2c-adapter/i2c-2/new_device
 ntpd -gq
 /etc/init.d/ntpd start
 hwclock.util-linux --systohc --utc -f /dev/rtc0
+echo '#!/bin/sh' > /etc/init.d/rtcenable.sh
+echo 'echo bq32000 0x68 | tee /sys/class/i2c-adapter/i2c-2/new_device' >> /etc/init.d/rtcenable.sh
+echo 'hwclock --hctosys -f /dev/rtc0' >> /etc/init.d/rtcenable.sh
+chmod 775 /etc/init.d/rtcenable.sh
+cd /etc/init.d/
+sudo update-rc.d rtcenable.sh defaults
